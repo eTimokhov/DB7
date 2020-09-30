49841be4b3d93cfbacf81eadab9cdf3932fdfc73
eTimokhov <e.timokhov@gmail.com>
2020-09-30 22:34:01 +0300

    Lab 2

---
+++ Lab2/V6_1.sql
@@ -0,0 +1,23 @@
+﻿--Вывести на экран самую раннюю дату начала работы сотрудника в каждом отделе. Дату вывести для каждого отдела.
+SELECT d.Name, MIN(edh.StartDate) as StartDate
+FROM HumanResources.Employee e
+JOIN HumanResources.EmployeeDepartmentHistory edh ON e.BusinessEntityID = edh.BusinessEntityID
+JOIN HumanResources.Department d ON edh.DepartmentID = d.DepartmentID
+GROUP BY d.Name;
+
+--Вывести на экран название смены сотрудников, работающих на позиции ‘Stocker’. Замените названия смен цифрами (Day — 1; Evening — 2; Night — 3).
+SELECT e.BusinessEntityID, e.JobTitle, CASE s.Name
+										 WHEN 'Day' THEN 1
+										 WHEN 'Evening' THEN 2
+										 WHEN 'Night' THEN 3
+									   END AS ShiftName
+FROM HumanResources.Employee e
+JOIN HumanResources.EmployeeDepartmentHistory edh ON e.BusinessEntityID = edh.BusinessEntityID
+JOIN HumanResources.Shift s ON edh.ShiftID = s.ShiftID
+WHERE e.JobTitle = 'Stocker';
+
+--Вывести на экран информацию обо всех сотрудниках, с указанием отдела, в котором они работают в настоящий момент. В названии позиции каждого сотрудника заменить слово ‘and’ знаком & (амперсанд).
+SELECT e.BusinessEntityID, REPLACE(e.JobTitle, 'and', '&') AS JobTitle, d.Name
+FROM HumanResources.Employee e
+JOIN HumanResources.EmployeeDepartmentHistory edh ON e.BusinessEntityID = edh.BusinessEntityID
+JOIN HumanResources.Department d ON edh.DepartmentID = d.DepartmentID;
---
+++ Lab2/V6_2.sql
@@ -0,0 +1,56 @@
+﻿--a) создайте таблицу dbo.Person с такой же структурой как Person.Person, кроме полей xml, uniqueidentifier, не включая индексы, ограничения и триггеры;
+CREATE TABLE dbo.Person (
+	BusinessEntityId INT NOT NULL,
+	PersonType NCHAR(2) NOT NULL,
+	NameStyle BIT NOT NULL,
+	Title NVARCHAR(8) NOT NULL,
+	FirstName NVARCHAR(50) NOT NULL,
+	MiddleName NVARCHAR(50) NULL,
+	LastName NVARCHAR(50) NOT NULL,
+	Suffix NVARCHAR(10) NULL,
+	EmailPromotion INT NOT NULL,
+	ModifiedDate DATETIME NOT NULL
+);
+
+--b) используя инструкцию ALTER TABLE, создайте для таблицы dbo.Person составной первичный ключ из полей BusinessEntityID и PersonType;
+ALTER TABLE dbo.Person
+ADD CONSTRAINT PK_Person PRIMARY KEY (BusinessEntityID, PersonType);
+
+--c) используя инструкцию ALTER TABLE, создайте для таблицы dbo.Person ограничение для поля PersonType, чтобы заполнить его можно было только значениями из списка ‘GC’,’SP’,’EM’,’IN’,’VC’,’SC’;
+ALTER TABLE dbo.Person
+ADD CONSTRAINT CHK_Person_PersonType CHECK (PersonType IN ('GC','SP','EM','IN','VC','SC'));
+
+--d) используя инструкцию ALTER TABLE, создайте для таблицы dbo.Person ограничение DEFAULT для поля Title, задайте значение по умолчанию ‘n/a’;
+ALTER TABLE dbo.Person
+ADD CONSTRAINT DF_Person_Title DEFAULT 'n/a' FOR Title;
+
+--e) заполните таблицу dbo.Person данными из Person.Person только для тех лиц, для которых тип контакта в таблице ContactType определен как ‘Owner’. Поле Title заполните значениями по умолчанию;
+INSERT INTO dbo.Person (
+	BusinessEntityID, 
+	PersonType, 
+	NameStyle, 
+	FirstName, 
+	MiddleName, 
+	LastName, 
+	Suffix,
+	EmailPromotion, 
+	ModifiedDate
+)
+SELECT 
+	p.BusinessEntityID,
+	p.PersonType,
+	p.NameStyle,
+	p.FirstName,
+	p.MiddleName,
+	p.LastName,
+	p.Suffix,
+	p.EmailPromotion,
+	p.ModifiedDate
+FROM Person.Person p
+INNER JOIN Person.BusinessEntityContact bec ON p.BusinessEntityID = bec.PersonID
+INNER JOIN Person.ContactType ct ON bec.ContactTypeID = ct.ContactTypeID
+WHERE ct.Name = 'Owner';
+
+--f) измените размерность поля Title, уменьшите размер поля до 4-ти символов, также запретите добавлять null значения для этого поля.
+ALTER TABLE dbo.Person
+ALTER COLUMN Title NVARCHAR(4) NOT NULL;
